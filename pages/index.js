3ca98d44c4afb663ba98f00ea91226da25085e3b
josh-may <joshmay222@gmail.com>
2025-01-07 14:48:00 -0600

    Lazy commit

--- pages/index.js
+++ pages/index.js
@@ -144,7 +144,7 @@ export default function Home() {
     <div className="min-h-screen bg-white">
       <section className="bg-white">
         <main className="max-w-7xl mx-auto px-4 py-5">
-          <div className="flex flex-col space-y-8">
+          <div className="flex flex-col space-y-5">
             <div className="max-w-4xl">
               <h1 className="text-3xl font-proxima-nova-bold text-black mb-6">
                 <strong>Custom Sock Builder</strong>

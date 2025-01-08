987698a7c1145c6f47c21e0d20e50be01d5fed6f
josh-may <joshmay222@gmail.com>
2025-01-08 11:00:42 -0600

    Lazy commit

--- pages/index.js
+++ pages/index.js
@@ -343,7 +343,7 @@ If the user prompt above isnt related to a sock design than return a sketch of a
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center p-4 z-50">
           <div
-            className="absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(0,0,0,0.8)_0%,rgba(0,0,0,0.6)_10%,rgba(255,255,255,0)_50%)]"
+            className="absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(0,0,0,0.8)_0%,rgba(0,0,0,0.6)_8%,rgba(255,255,255,0)_30%)]"
             onClick={() => setShowModal(false)}
           ></div>
 
